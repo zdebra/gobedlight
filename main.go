f52511fcd467ddf96feb83a42d0e261c12f1b614
Zdenek <zd.brabec@gmail.com>
2017-03-08 22:01:10 +0100

    first test

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/stianeikeland/go-rpio"
+	"time"
 )
 
 func main()  {
-	fmt.Println("hello")
+	fmt.Println("Start awesome light")
+	err := rpio.Open()
+	pin := rpio.Pin(12)
+
+	pin.Output()       // Output mode
+	pin.High()         // Set pin High
+	time.Sleep(1000 * time.Millisecond)
+	pin.Low()          // Set pin Low
+	pin.Toggle()       // Toggle pin (Low -> High -> Low)
+
+	rpio.Close()
+	
+
 }
