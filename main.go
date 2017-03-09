7cba8ee1558852f35563adb46dba88478b39fc4d
Zdenek <zd.brabec@gmail.com>
2017-03-09 20:45:59 +0100

    fx

--- main.go
+++ main.go
@@ -14,16 +14,14 @@ func main()  {
 	}
 	pin := rpio.Pin(18)
 
-	//pin.Output()       // Output mode
-	//pin.High()         // Set pin High
-
-	pin.PullUp()
+	pin.Output()       // Output mode
+	pin.High()         // Set pin High
 	time.Sleep(2000 * time.Millisecond)
 	pin.Low()          // Set pin Low
 	fmt.Println("turning it off")
 
 	time.Sleep(2000 * time.Millisecond)
-	//rpio.Close()
-	pin.PullOff()
+	rpio.Close()
+
 
 }
